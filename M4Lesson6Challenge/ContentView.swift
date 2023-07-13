16ce957eb4d6be546ce438c3c4e7effe23f502ea
Chuck Belcher <cfbelcher@pateogroup.com>
2023-07-13 09:28:48 -0400

    Initial Commit

---
+++ M4Lesson6Challenge/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  M4Lesson6Challenge
+//
+//  Created by Chuck Belcher on 7/13/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ M4Lesson6Challenge/M4Lesson6ChallengeApp.swift
@@ -0,0 +1,17 @@
+//
+//  M4Lesson6ChallengeApp.swift
+//  M4Lesson6Challenge
+//
+//  Created by Chuck Belcher on 7/13/23.
+//
+
+import SwiftUI
+
+@main
+struct M4Lesson6ChallengeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
